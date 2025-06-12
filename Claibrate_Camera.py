13167096a91d3510f39355e1630e65d6f5d1f385
DHall29 <DHall29@its.jnj.com>
2025-06-12 09:38:13 -0400

    Calibration Complete

--- Claibrate_Camera.py
+++ Claibrate_Camera.py
@@ -1,53 +1,59 @@
-import cv2
 import numpy as np
-
-def find_chessboard_corners(images, pattern_size):
-    obj_points = []
-    img_points = []
-
-    # Prepare the 3D points of the chessboard corners (0,0,0), (1,0,0), (2,0,0) ..., (6,5,0)
-    objp = np.zeros((pattern_size[0] * pattern_size[1], 3), np.float32)
-    objp[:, :2] = np.mgrid[0:pattern_size[0], 0:pattern_size[1]].T.reshape(-1, 2)
-
-    for img in images:
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        ret, corners = cv2.findChessboardCorners(gray, pattern_size, None)
-
-        if ret:
-            obj_points.append(objp)
-            img_points.append(corners)
-
-    return obj_points, img_points
-
-def calibrate_camera(obj_points, img_points, img_size):
-    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(obj_points, img_points, img_size, None, None)
-    return ret, mtx, dist, rvecs, tvecs
-
-def undistort_image(img, mtx, dist):
-    undistorted_img = cv2.undistort(img, mtx, dist, None, mtx)
-    return undistorted_img
-
-
+import cv2 as cv
 import glob
+ 
+# termination criteria
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+ 
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((6*7,3), np.float32)
+objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+ 
+# Arrays to store object points and image points from all the images.
+objpoints = [] # 3d point in real world space
+imgpoints = [] # 2d points in image plane.
+ 
+images = glob.glob('C:/Users/DHall29/workspace/Vision/images/*.jpg')
+ 
+for fname in images:
+    img = cv.imread(fname)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+ 
+    # Find the chess board corners
+    ret, corners = cv.findChessboardCorners(gray, (7,6), None)
+ 
+    # If found, add object points, image points (after refining them)
+    if ret == True:
+        objpoints.append(objp)
+ 
+        corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
+        imgpoints.append(corners2)
+ 
+        # Draw and display the corners
+        cv.drawChessboardCorners(img, (7,6), corners2, ret)
+        cv.imshow('img', img)
+        cv.waitKey(500)
+cv.destroyAllWindows()
+ 
+ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 
-# Load the images
-image_files = glob.glob('C:/Users/DHall29/workspace/Vision/images/*.jpg')
-images = [cv2.imread(file) for file in image_files]
+img = cv.imread('C:/Users/DHall29/workspace/Vision/images/image12.jpg')
+h,  w = img.shape[:2]
+newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
 
-# Find the chessboard corners
-pattern_size = (9, 6)
-obj_points, img_points = find_chessboard_corners(images, pattern_size)
+# undistort
+dst = cv.undistort(img, mtx, dist, None, newcameramtx)
 
-# Calibrate the camera
-img_size = (images[0].shape[1], images[0].shape[0])
-ret, mtx, dist, rvecs, tvecs = calibrate_camera(obj_points, img_points, img_size)
+# crop the image
+x, y, w, h = roi
+dst = dst[y:y+h, x:x+w]
+cv.imwrite('calibresult.png', dst)
 
-# Undistort an example image
-undistorted_example = undistort_image(images[0], mtx, dist)
 
-# Show the original and undistorted images
-cv2.imshow('Original Image', images[0])
-cv2.imshow('Undistorted Image', undistorted_example)
+mean_error = 0
+for i in range(len(objpoints)):
+    imgpoints2, _ = cv.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
+    error = cv.norm(imgpoints[i], imgpoints2, cv.NORM_L2)/len(imgpoints2)
+    mean_error += error
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+print( "total error: {}".format(mean_error/len(objpoints)) )
