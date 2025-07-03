7f1369fd53e61ece289bf16afcf4f81f2b0697c7
marcelo <marcelo.ijk>
2025-07-03 16:39:55 +0200

    Minor

--- tracking_app.py
+++ tracking_app.py
@@ -87,7 +87,7 @@ while True:
 
     current_pointer_pos_img = None
     
-    if not ids:
+    if not ids.any():
         #print("References on bone and/or pointer were not detected.")
         continue
     
