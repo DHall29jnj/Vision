17392bce8759fc4591b3c1edcdc230b8c3fb6f60
DHall29jnj <DHall29@its.jnj.com>
2025-07-14 07:23:34 -0400

    assets folder

--- app/core/config.py
+++ app/core/config.py
@@ -33,7 +33,7 @@ class Config:
 
         self.dist_coeffs = dist_coeffs
 
-        self.marker_size = 0.05  # In meters (used for pose estimation)
+        self.marker_size = 0.05  # In meters 
 
         self.pntr_id = 1
         self.ref_id = 2
